5e0fee2dc4c8bb59c8f9e1679366c058f299122b
Adam Jonas <adamjonas25@gmail.com>
2021-01-18 09:30:45 +0000

    substrings pass all tests

--- ruby/sub-strings/spec/sub_strings_spec.rb
+++ ruby/sub-strings/spec/sub_strings_spec.rb
@@ -1,7 +1,34 @@
 require_relative './../sub_strings'
 
 describe '#substrings' do
-  it "is a method and can be passed a string as argument" do
-    expect { sub_strings('') }.to_not raise_error
+  let (:dictionary) { ["below","down","go","going","horn", "how","howdy","it","i","low","own",
+                       "part","partner","sit"] }
+
+  let (:solution_hash) { {"down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2,
+                       "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1} }
+
+  let (:tale_of_two_cities) { %w[It was the best of times, it was the worst of times,
+                                 it was the age of wisdom, it was the age of foolishness,
+                                 it was the epoch of belief, it was the epoch of incredulity,
+                                 it was the season of light, it was the season of darkness,
+                                 it was the spring of hope, it was the winter of despair.]}
+
+  let (:dickens_string) { "it sit pair despair, the age of forage,
+                           as dark as night as light as summer" }
+
+  it "can be passed 2 arguments" do
+    expect { substrings('', []) }.to_not raise_error
+  end
+
+  it 'can handle 1 word string' do
+    expect(substrings('below', dictionary)).to eq 'below'=>1, 'low'=>1
+  end
+
+  it 'can handle a longer string' do
+    expect(substrings("Howdy partner, sit down! How's it going?", dictionary)).to eq solution_hash
+  end
+
+  it 'can handle Dickens' do
+    expect(substrings(dickens_string, tale_of_two_cities)).to eq 'the'=>10,'of'=>10,'it'=>18,'age'=>4
   end
 end
--- ruby/sub-strings/sub_strings.rb
+++ ruby/sub-strings/sub_strings.rb
@@ -1,3 +1,13 @@
-def sub_strings(string)
+def substrings(string, dictionary)
+  p words = string.downcase.split
+  hash = {}
 
+  dictionary.each do |dict_word|
+    words.each do |string_word|
+      if string_word.include?(dict_word)
+        hash.include?(dict_word) ? hash[dict_word] += 1 : hash[dict_word] = 1
+      end
+    end
+  end
+  hash
 end
