2a32905bd141ce04f5bf1431a2fc26690a9de8b8
Adam Jonas <adamjonas25@gmail.com>
2021-01-18 15:26:55 +0000

    #bubble_sort pass 3 specs

--- ruby/bubble-sort/bubble_sort.rb
+++ ruby/bubble-sort/bubble_sort.rb
@@ -1,5 +1,12 @@
 class Array
   def bubble_sort
+    self.each_with_index do |element, index|
+      return self if self[index + 1].nil?  
+      if element > self[index + 1]
+        self[index] = self[index + 1]
+        self[index + 1] = element
+      end
+    end
     self
   end
 end
--- ruby/bubble-sort/spec/bubble_sort_spec.rb
+++ ruby/bubble-sort/spec/bubble_sort_spec.rb
@@ -4,8 +4,12 @@ describe '#bubble_sort' do
   it "is an array method" do
     expect([].respond_to?(:bubble_sort)).to be true
   end
-  
+
   it "returns [] given an empty array" do
     expect([].bubble_sort).to eq []
+  end
+
+  it 'can sort 2 elements' do
+    expect([2,1].bubble_sort).to eq [1,2]
   end
 end
