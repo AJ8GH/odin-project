7b9bd70302197280d8c397cc469e035525e67048
Adam Jonas <adamjonas25@gmail.com>
2021-01-18 10:35:13 +0000

    stock_picker pass all specs, rename variables to be clearer

--- ruby/stock-picker/spec/stock_picker_spec.rb
+++ ruby/stock-picker/spec/stock_picker_spec.rb
@@ -1,6 +1,9 @@
 require_relative './../stock_picker'
 
 describe '#stock_prices' do
+  let (:big_array) { [2,7,23,4,6,8,8,4,234,234,234,23,2,21,1,2,5,6,7,7,78,89,9,9,90,234,
+                      23,3,5,6,65,5,4,5,76,756,68,68,78,78,89,9,9090,909,5,43,3,3,22,3] }
+
   it "takes an array" do
     expect {stock_picker([])}.to_not raise_error
   end
@@ -19,5 +22,9 @@ describe '#stock_prices' do
 
   it "can handle the example case" do
     expect(stock_picker([17,3,6,9,15,8,6,1,10])).to eq [1,4]
+  end
+
+  it "can handle larger arrays" do
+    expect(stock_picker(big_array)).to eq [14,42]
   end
 end
--- ruby/stock-picker/stock_picker.rb
+++ ruby/stock-picker/stock_picker.rb
@@ -3,19 +3,17 @@ def stock_picker(array)
   sell_index = 0
   profit = 0
 
-  array.each_with_index do |price, index|
-    array.each_with_index do |p2, i2|
-      potential_profit = price - p2
-      unless i2 <= index
+  array.each_with_index do |buy_price, bp_index|
+    array.each_with_index do |sell_price, sp_index|
+      potential_profit = buy_price - sell_price
+      unless sp_index <= bp_index
         if potential_profit < profit
           profit = potential_profit
-          buy_index = index
-          sell_index = i2
+          buy_index = bp_index
+          sell_index = sp_index
         end
       end
     end
   end
   [buy_index, sell_index]
 end
-
-# stock_picker
